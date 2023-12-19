ae21dea6105fc5e0c9fcd14295e54f0b9490d794
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2023-12-19 01:22:08 +0100

    Add day03 part 2

--- 2023/day03/src/main.rs
+++ 2023/day03/src/main.rs
@@ -2,13 +2,20 @@ use regex::Regex;
 
 fn main() {
     let input = std::fs::read_to_string("src/input").unwrap();
-    part1(&input);
+    let (part_numbers, symbols) = parse_data(&input);
+    part1(&part_numbers, &symbols);
+    part2(&part_numbers, &symbols);
 }
 
 #[derive(Debug)]
 struct Location {
     x: usize,
     y: usize,
+}
+
+struct Symbol {
+    loc: Location,
+    symbol: char,
 }
 
 #[derive(Debug)]
@@ -30,7 +37,7 @@ impl PartNumber {
     }
 }
 
-fn part1(input: &str) {
+fn parse_data(input: &str) -> (Vec<PartNumber>, Vec<Symbol>) {
     let int_regex = Regex::new(r"\d+").unwrap();
     let part_numbers: Vec<PartNumber> = input.lines()
         .enumerate()
@@ -45,15 +52,22 @@ fn part1(input: &str) {
         .collect();
 
     let symbol_regex = Regex::new(r"[^\d.]").unwrap();
-    let symbols: Vec<Location> = input.lines()
+    let symbols: Vec<Symbol> = input.lines()
         .enumerate()
         .map(|(y, line)|
-            symbol_regex.find_iter(line).map(move |m| Location { x: m.start(), y })
+            symbol_regex.find_iter(line).map(move |m| Symbol {
+                loc: Location { x: m.start(), y },
+                symbol: m.as_str().as_bytes()[0] as char,
+            })
         ).flatten()
         .collect();
 
+    (part_numbers, symbols)
+}
+
+fn part1(part_numbers: &Vec<PartNumber>, symbols: &Vec<Symbol>) {
     let part_num_sum: usize = part_numbers.iter().filter_map(|part_number| {
-        if symbols.iter().any(|symbol_loc| part_number.is_adjacent_to(symbol_loc)) {
+        if symbols.iter().any(|symbol| part_number.is_adjacent_to(&symbol.loc)) {
             Some(part_number.number)
         } else {
             None
@@ -61,4 +75,27 @@ fn part1(input: &str) {
     }).sum();
 
     println!("Part 1: {part_num_sum}");
+}
+
+
+fn part2(part_numbers: &Vec<PartNumber>, symbols: &Vec<Symbol>) {
+    let gear_ratio_sums: usize = symbols.iter()
+        .filter(|symbol| symbol.symbol == '*')
+        .map(|symbol| {
+            let adjacent_part_numbers: Vec<usize> = part_numbers.iter()
+                .filter_map(|part_number| {
+                    if part_number.is_adjacent_to(&symbol.loc) {
+                        Some(part_number.number)
+                    } else {
+                        None
+                    }
+                }).collect();
+            if adjacent_part_numbers.len() == 2 {
+                adjacent_part_numbers.iter().product()
+            } else {
+                0
+            }
+        }).sum();
+
+    println!("Part 2: {gear_ratio_sums}");
 }
