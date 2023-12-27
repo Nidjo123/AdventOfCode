a29c6eeb5d5d3a44e55dce632eb6e2424cf802bb
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2023-12-27 17:52:07 +0100

    Add day08 part 2

--- 2023/day08/src/main.rs
+++ 2023/day08/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
 use regex::Regex;
@@ -7,6 +7,7 @@ fn main() {
     let input = fs::read_to_string("src/input").unwrap();
     let data = parse_data(&input);
     part1(&data);
+    part2(&data);
 }
 
 enum Direction {
@@ -14,6 +15,7 @@ enum Direction {
     LEFT,
 }
 
+#[derive(Debug)]
 struct Node {
     pub name: String,
     pub left: String,
@@ -78,4 +80,135 @@ fn part1(data: &Data) {
         }
     }
     println!("Part 1: {steps}");
+}
+
+struct Path {
+    steps: Vec<String>,
+}
+
+impl Path {
+    fn get_cycle_lengths(&self) -> Option<usize> {
+        let mut visited_names = HashSet::new();
+        for (idx, name) in self.steps.iter().enumerate() {
+            if is_end_node(name) && visited_names.contains(name) {
+                let first_pos = self.steps.iter().position(|x| x == name).unwrap();
+                let length = idx - first_pos;
+                let expected_next_cycle_start = idx + length;
+                if expected_next_cycle_start < self.steps.len()
+                    && self
+                        .steps
+                        .iter()
+                        .skip(idx)
+                        .take(length)
+                        .zip(self.steps.iter().skip(first_pos).take(length))
+                        .all(|(x, y)| x == y)
+                {
+                    return Some(length);
+                }
+            }
+            visited_names.insert(name);
+        }
+        None
+    }
+}
+
+fn is_end_node(name: &str) -> bool {
+    name.ends_with("Z")
+}
+
+fn add_path_steps(nodes: &Vec<&Node>, paths: &mut Vec<Path>) {
+    nodes
+        .iter()
+        .zip(paths.iter_mut())
+        .for_each(|(&node, path)| path.steps.push(node.name.to_owned()));
+}
+
+fn prime_factors(mut n: usize) -> Vec<usize> {
+    let original_n = n;
+    let mut factors = Vec::new();
+    let mut factor = 2;
+    while n % factor == 0 {
+        n /= factor;
+        factors.push(factor);
+    }
+    factor = 3;
+    while n > 1 {
+        while n % factor == 0 {
+            n /= factor;
+            factors.push(factor);
+        }
+        factor += 2;
+    }
+    assert_eq!(factors.iter().product::<usize>(), original_n);
+    factors
+}
+
+fn lcm(numbers: &Vec<usize>) -> usize {
+    let mut prime_factor_counts: HashMap<usize, usize> = HashMap::new();
+    for factors in numbers.iter().map(|&x| prime_factors(x)) {
+        let mut counts = HashMap::new();
+        for factor in factors {
+            *counts.entry(factor).or_insert(0) += 1;
+        }
+        for (factor, count) in counts {
+            let entry = prime_factor_counts.entry(factor).or_insert(count);
+            *entry = count.max(*entry);
+        }
+    }
+    let mut res = 1;
+    for (factor, count) in prime_factor_counts {
+        res *= factor.pow(count as u32);
+    }
+    res
+}
+
+#[test]
+fn test_lcm() {
+    assert_eq!(lcm(&vec![1]), 1);
+    assert_eq!(lcm(&vec![1, 2, 3]), 6);
+    assert_eq!(lcm(&vec![6, 2, 3]), 6);
+    assert_eq!(lcm(&vec![5]), 5);
+    assert_eq!(lcm(&vec![2, 3, 7]), 42);
+}
+
+fn part2(data: &Data) {
+    let node_map = build_node_map(&data.nodes);
+    let mut steps = 0;
+    let mut nodes: Vec<_> = data
+        .nodes
+        .iter()
+        .filter(|node| node.name.ends_with("A"))
+        .collect();
+
+    let mut paths: Vec<Path> = Vec::with_capacity(nodes.len());
+    for _ in &nodes {
+        paths.push(Path { steps: Vec::new() });
+    }
+    add_path_steps(&nodes, &mut paths);
+
+    for direction in data.directions.iter().cycle() {
+        let mut new_nodes = nodes
+            .iter()
+            .map(|node| match direction {
+                Direction::RIGHT => *node_map.get(&node.right).unwrap(),
+                Direction::LEFT => *node_map.get(&node.left).unwrap(),
+            })
+            .collect();
+        nodes.clear();
+        nodes.append(&mut new_nodes);
+        steps += 1;
+        add_path_steps(&nodes, &mut paths);
+        if steps % 10000 == 0 {
+            let cycle_lengths: Vec<_> = paths.iter().map(Path::get_cycle_lengths).collect();
+            if cycle_lengths.iter().all(Option::is_some) {
+                let cycle_lengths: Vec<_> = cycle_lengths.iter().map(|x| x.unwrap()).collect();
+                let cycle_length_lcm = lcm(&cycle_lengths);
+                println!("Part 2: {cycle_length_lcm}");
+                break;
+            }
+        }
+        if nodes.iter().all(|&node| is_end_node(&node.name)) {
+            break;
+        }
+    }
 }
