584262fb62409a7a0e90e69b59fdee7f1f8637d0
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2024-01-05 16:22:03 +0100

    day14 WIP

--- 2023/day14/src/main.rs
+++ 2023/day14/src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 fn main() {
     let map = read_map("input");
     part1(&map);
+    part2(&map);
 }
 
 fn read_map(name: &str) -> Map {
@@ -25,6 +26,13 @@ enum Direction {
     South,
     West,
 }
+
+const CYCLE: [Direction; 4] = [
+    Direction::North,
+    Direction::West,
+    Direction::South,
+    Direction::East,
+];
 
 struct Map {
     rows: Vec<Vec<TileType>>,
@@ -64,6 +72,36 @@ impl Map {
             .collect()
     }
 
+    fn get_search_range(&self, direction: &Direction) -> Box<dyn Iterator<Item = usize>> {
+        match &direction {
+            Direction::North => Box::new(0..self.num_rows()),
+            Direction::East => Box::new((0..self.num_cols()).rev()),
+            Direction::South => Box::new((0..self.num_rows()).rev()),
+            Direction::West => Box::new(0..self.num_cols()),
+        }
+    }
+
+    fn update_state(vec: &mut Vec<TileType>, empty_idx: &mut Option<usize>, idx: usize) -> bool {
+        match vec[idx] {
+            TileType::CubeRock => {
+                empty_idx.take();
+                false
+            }
+            TileType::Empty => {
+                empty_idx.get_or_insert(idx);
+                false
+            }
+            TileType::RoundedRock => {
+                if let Some(empty_idx) = empty_idx {
+                    vec.swap(idx, *empty_idx);
+                    true
+                } else {
+                    false
+                }
+            }
+        }
+    }
+
     fn get_tilted_map(&self, direction: &Direction) -> Self {
         let mut rows = self.rows.clone();
         if direction == &Direction::North || direction == &Direction::South {
@@ -72,21 +110,10 @@ impl Map {
                 loop {
                     let mut swapped = false;
                     let mut empty_row_idx = None;
-                    for row_idx in 0..self.num_rows() {
-                        match col[row_idx] {
-                            TileType::CubeRock => {
-                                empty_row_idx.take();
-                            }
-                            TileType::Empty => {
-                                empty_row_idx.get_or_insert(row_idx);
-                            }
-                            TileType::RoundedRock => {
-                                if let Some(empty_row_idx) = empty_row_idx {
-                                    col.swap(row_idx, empty_row_idx);
-                                    swapped = true;
-                                    break;
-                                }
-                            }
+                    for row_idx in self.get_search_range(direction) {
+                        if Self::update_state(&mut col, &mut empty_row_idx, row_idx) {
+                            swapped = true;
+                            break;
                         }
                     }
                     if !swapped {
@@ -98,9 +125,35 @@ impl Map {
                 }
             }
         } else {
+            for row_idx in 0..self.num_rows() {
+                let mut row = self.rows[row_idx].clone();
+                loop {
+                    let mut swapped = false;
+                    let mut empty_col_idx = None;
+                    for col_idx in self.get_search_range(direction) {
+                        if Self::update_state(&mut row, &mut empty_col_idx, col_idx) {
+                            swapped = true;
+                            break;
+                        }
+                    }
+                    if !swapped {
+                        break;
+                    }
+                }
+                rows[row_idx].swap_with_slice(&mut row[..]);
+            }
         }
 
         Self { rows }
+    }
+
+    fn get_cycle_tilted(&self) -> Self {
+        CYCLE
+            .iter()
+            .skip(1)
+            .fold(self.get_tilted_map(&CYCLE[0]), |map, direction| {
+                map.get_tilted_map(direction)
+            })
     }
 
     fn calculate_load(&self, direction: &Direction) -> usize {
@@ -113,6 +166,8 @@ impl Map {
                     }
                 }
             }
+        } else {
+            todo!("only implemented for North")
         }
         load
     }
@@ -133,8 +188,37 @@ impl Map {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{read_map, Direction, CYCLE};
+
+    #[test]
+    fn test_example_part1() {
+        let map = read_map("test");
+        let north_tilted_map = map.get_tilted_map(&Direction::North);
+        assert_eq!(north_tilted_map.calculate_load(&Direction::North), 136);
+    }
+
+    #[test]
+    fn test_example_part2() {
+        let mut map = read_map("test");
+        map = map.get_cycle_tilted();
+        assert_eq!(map.calculate_load(&Direction::North), 87);
+        map = map.get_cycle_tilted();
+        assert_eq!(map.calculate_load(&Direction::North), 69);
+        map = map.get_cycle_tilted();
+        assert_eq!(map.calculate_load(&Direction::North), 69);
+    }
+}
+
 fn part1(map: &Map) {
     let north_tilted_map = map.get_tilted_map(&Direction::North);
     let load = north_tilted_map.calculate_load(&Direction::North);
     println!("Part 1: {load}");
+}
+
+fn part2(map: &Map) {
+    let north_tilted_map = map.get_tilted_map(&Direction::North);
+    let load = north_tilted_map.calculate_load(&Direction::North);
+    println!("Part 2: {load}");
 }
