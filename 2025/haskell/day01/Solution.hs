cc68f6d578f2a4e0479cfb12f05fad6dc181d692
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2025-12-02 22:15:52 +0100

    Day02 part 2

--- 2025/haskell/day01/Solution.hs
+++ 2025/haskell/day01/Solution.hs
@@ -20,10 +20,29 @@ calculatePositions pos (m:ms) = nextPos : calculatePositions nextPos ms
     where
       nextPos = nextPosition m pos
 
+leftZeros :: Int -> Int -> Int -> (Int, Int)
+leftZeros currPos 0 zeros = (currPos, zeros)
+leftZeros currPos steps zeros = leftZeros ((currPos - 1) `mod` 100) (steps - 1) (zeros + if currPos == 1 then 1 else 0)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+nextState :: (Int, Int) -> Move -> (Int, Int)
+nextState (zs, currPos) move = (zs + abs turns, nextPos)
+    where
+      (turns, nextPos) = case move of
+        L steps -> swap $ leftZeros currPos steps 0
+        R steps -> (currPos + steps) `divMod` 100
+
+calculatePassword2 :: Int -> [Move] -> (Int, Int)
+calculatePassword2 startPos = foldl nextState (if startPos == 0 then 1 else 0, startPos)
+
 main = do
     contents <- getContents
     let moves = map fromStr $ lines contents
         positions = calculatePositions 50 moves
-        password = length $ filter (== 0) positions
-    putStrLn $ show password
+        password1 = length $ filter (== 0) positions
+        password2 = fst $ calculatePassword2 50 moves
+    putStrLn $ show password1
+    putStrLn $ show password2
 
