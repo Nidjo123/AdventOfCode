ddc01ac6947d6466aa55446e08250ab12c164533
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2025-12-03 22:02:09 +0100

    Day02 part 2

--- 2025/haskell/day02/Solution.hs
+++ 2025/haskell/day02/Solution.hs
@@ -18,12 +18,31 @@ isInvalidId x = left == right
       strX = show x
       (left, right) = splitAt (length strX `div` 2) strX
 
-invalidIds :: Range -> [Integer]
-invalidIds (Range low high) = filter isInvalidId [low..high]
+invalidIds :: (Integer -> Bool) -> Range -> [Integer]
+invalidIds filtPred (Range low high) = filter filtPred [low..high]
+
+allEqual :: (Eq a) => [a] -> Bool
+allEqual [] = True
+allEqual (x:[]) = True
+allEqual (x:xs) = x == head xs && allEqual xs
+
+substrings :: String -> Int -> [String]
+substrings "" _  = []
+substrings s len = s1 : substrings rest len
+    where
+      (s1, rest) = splitAt len s
+
+isInvalidId2 :: Integer -> Bool
+isInvalidId2 x = any allEqual $ map (substrings strX) [len | len <- [1..lenX `div` 2], lenX `mod` len == 0]
+    where
+      strX = show x
+      lenX = length strX
 
 main = do
     line <- getContents
     let parts = splitOn (== ',') line
         ranges = map fromStr parts
-        invalidIdsSum = sum . concat $ map invalidIds ranges
-    putStrLn $ show invalidIdsSum
+        invalidIdsSum1 = sum . concat $ map (invalidIds isInvalidId) ranges
+        invalidIdsSum2 = sum . concat $ map (invalidIds isInvalidId2) ranges
+    putStrLn $ show invalidIdsSum1
+    putStrLn $ show invalidIdsSum2
