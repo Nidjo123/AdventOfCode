d727530ab7ba4be299cbc0b1c9a6431d6389e4c5
Nikola Bunjevac <nikola.bunjevac@gmail.com>
2025-12-09 00:28:18 +0100

    Day06 part 2

--- 2025/haskell/day06/Solution.hs
+++ 2025/haskell/day06/Solution.hs
@@ -7,6 +7,19 @@ getOp c = case c of
   '*' -> (*)
   _ -> error "unknown operation"
 
+splitBy :: (a -> Bool) -> [a] -> [[a]]
+splitBy p xs = splitBy' p xs [] []
+    where
+      splitBy' p [] res buf = reverse (buf : res)
+      splitBy' p (x:xs) res buf = if p x then splitBy' p xs (buf : res) [] else splitBy' p xs res (buf ++ [x])
+
+solve2 :: [String] -> Integer
+solve2 xs = foldr op initNum tailNums
+  where
+    op = getOp . last . head $ xs
+    initNum = read . init . head $ xs
+    tailNums = map read . tail $ xs
+
 main = do
   contents <- getContents
   let
@@ -16,5 +29,9 @@ main = do
       $ map (\(op, nums) -> foldr op (head nums) (tail nums)) 
       $ zip operations 
       $ transpose table
+    part2 = sum
+      $ map solve2
+      $ splitBy (null . words) . transpose
+      $ lines contents
   print part1
-
+  print part2
